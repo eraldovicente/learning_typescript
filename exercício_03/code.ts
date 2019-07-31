0f2c275730da8bf836bb0bd4e7988e36a4929e42
eraldovicente <eraldovicente2@gmail.com>
2019-07-31 13:26:13 -0300

    exercício3

---
+++ exercício_03/code.ts
@@ -0,0 +1,42 @@
+function executar() {
+    //var valor = "Eraldo";
+
+    /*
+     O compilador TypeScript emite um erro ao tentar
+     atribuir um number a uma variável que já foi inicializada
+     com string
+    */
+
+    //valor = 43;
+
+    /* Tipagem  com booleano */
+
+    //let valor: boolean = false;
+
+    /* '' com number */
+
+    //let numero: number = 10;
+    //let numero: number = 10.25; 
+    //let numero: number = 0xA;
+    //let numero: number = 0b1100100;
+    //let numero: number = 0o144;
+
+    /* Aceita aspas dupas ou simples */
+
+    //let valor: string = 'Eraldo';
+    //let valor: string = "Eraldo";
+
+    //let first_name = 'Eraldo';
+    //let full_name = first_name + " Vicente";
+    /* Template string */
+    //let full_name = `${first_name} Vicente`;
+
+    /* Arrays */
+
+    let nomes: string[] = ['Eraldo', 'Ana', 'Carlos'];
+    let apelidos: Array<string> = ['Silva', 'Vicente', 'Oliveira'];
+
+    /* Convertendo para string */
+
+    document.getElementById('info').innerHTML = `${nomes[0]} ${apelidos[1]}`;
+}
